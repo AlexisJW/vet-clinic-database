6c49d6bed788583e356f068a2bd134167981639c
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-03-29 14:50:26 -0400

    Insert data to species table.

--- data.sql
+++ data.sql
@@ -22,3 +22,7 @@ VALUES ('Sam Smith', 34),
        ('Melody Pond', 77),
        ('Dean Winchester', 14),
        ('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES ('Pokemon'),
+       ('Digimon');
