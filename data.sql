bca1550ad9fce7481cddfb8207ecdc7e6476afd6
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-03-29 15:22:59 -0400

    Modify my inserted animals to include Melody Pond owns Charmander, Squirtle, and Blossom.

--- data.sql
+++ data.sql
@@ -30,3 +30,4 @@ VALUES ('Pokemon'),
 UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
 UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
 UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
+UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
