a1a2d54df3fa0bdc6e55c73f193697f83a926177
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-03-30 13:34:18 -0400

    Insert data for vets table.

--- data.sql
+++ data.sql
@@ -40,3 +40,9 @@ UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon' OR name = 'Pikachu';
 UPDATE animals SET owner_id = 3 WHERE name = 'Devimon' OR name = 'Plantmon';
 UPDATE animals SET owner_id = 4 WHERE name = 'Charmander' OR name = 'Squirtle' OR name = 'Blossom';
 UPDATE animals SET owner_id = 5 WHERE name = 'Angemon' OR name = 'Boarmon';
+
+INSERT INTO vets (name, age, date_of_graduation)
+VALUES ('William Tatcher', 45, DATE '2000-04-23'),
+       ('Maisy Smith', 26, DATE '2019-01-17'),
+       ('Stephanie Mendez', 64, DATE '1981-05-04'),
+       ('Jack Harkness', 38, DATE '2008-06-08');
